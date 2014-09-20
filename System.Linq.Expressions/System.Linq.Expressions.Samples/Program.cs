3f0f04f58d66e8344d724d961f1fb8e326b2f07c
HorevIvan <HorevIvan@yandex.ru>
2014-09-20 21:16:53 +0800

    Добавлен проект System.Linq.Expressions.Samples

---
+++ System.Linq.Expressions/System.Linq.Expressions.Samples/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Linq.Expressions.Samples
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+        }
+    }
+}
