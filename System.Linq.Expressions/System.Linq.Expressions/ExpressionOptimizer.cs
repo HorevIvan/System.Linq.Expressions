50b3e5a45baf82a5ea11d944d445ee32fb21cd22
HorevIvan <HorevIvan@yandex.ru>
2014-09-21 11:58:21 +0800

    :/

---
+++ System.Linq.Expressions/System.Linq.Expressions/ExpressionOptimizer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Linq.Expressions
+{
+    public class ExpressionOptimizer
+    {
+        public ExpressionTree Tree { private set; get; }
+
+        public ExpressionOptimizer(Expression expression)
+        {
+            Tree = (new ExpressionTree(expression));
+        }
+
+        public IEnumerable<ExpressionTree> GetDublicateNodes()
+        {
+            return Tree.GetAllNodes()
+                .GroupBy(node => node)
+                .Where(node => node.Skip(1).Any())
+                .SelectMany(node => node);
+        }
+    }
+}
