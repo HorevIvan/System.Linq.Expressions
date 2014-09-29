452f5f940934d832d51517aab6bc86b3183d280c
horevivan <horevivan@yandex.ru>
2014-09-29 11:16:48 +0800

    Третья попытка

---
+++ System.Linq.Expressions/v3/ExtensionMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class ExtensionMethods
+{
+    public static T To<T>(this Object value)
+    {
+        return (T)(value);
+    }
+
+    public static Boolean Not(this Boolean value)
+    {
+        return !(value);
+    }
+
+    public static Boolean IsNull(this Object value)
+    {                                                              
+        return Object.ReferenceEquals(null, value);
+    }
+
+    public static Boolean Is<T>(this Object value)
+    {
+        return (value is T);
+    }
+
+    public static String Set(this String pattern, params Object[] parameters)
+    {
+        return String.Format(pattern, parameters);
+    }
+}
